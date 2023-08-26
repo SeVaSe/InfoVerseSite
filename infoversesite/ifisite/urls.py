dc72ac7dc830d7a0e51e1a7e46cd5fb107dc173b
sevase <kotaseva8@gmail.com>
2023-08-26 03:11:27 +0300

    TIK-001: Настроен первоначальный стиль, шапка, конфиг. О господи , я сделал это чудо, о дааааа

--- infoversesite/ifisite/urls.py
+++ infoversesite/ifisite/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('', main_page),  # 127.0.0.1:8000/
-    path('commands/', commands),  # 127.0.0.1:8000/commands/
-    path('feedback/', feedback),  # 127.0.0.1:8000/feedback/
-    path('payment/', payment)  # 127.0.0.1:8000/payment/
+    path('', main_page, name='main_p'),  # 127.0.0.1:8000/
+    path('commands/', commands, name='commands_p'),  # 127.0.0.1:8000/commands/
+    path('feedback/', feedback, name='feedback_p'),  # 127.0.0.1:8000/feedback/
+    path('payment/', payment, name='payment_p')  # 127.0.0.1:8000/payment/
 ]
