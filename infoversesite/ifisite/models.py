3989a66d6004f64e97aeced297f64a03c773254a
sevase <kotaseva8@gmail.com>
2023-08-27 00:41:10 +0300

    / - исправил траблы с гитом

--- infoversesite/ifisite/models.py
+++ infoversesite/ifisite/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 
-class Command(models.Model):
-    command = models.CharField(max_length=10000000)
-    descr = models.CharField(max_length=10000000)
-
-    def __str__(self):
-        return self.command  # Или какое-то другое представление объекта
-
-
