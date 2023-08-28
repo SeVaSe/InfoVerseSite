16a2b3ad4677368fa51818986fb15c50f7122dd1
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-08-28 22:45:32 +0300

    TIK-001: бд добавлена

--- infoversesite/ifisite/models.py
+++ infoversesite/ifisite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class CommandDescr(models.Model):
+    command = models.CharField(max_length=500)
+    text_descr = models.TextField(blank=True)
+    text_osob1 = models.TextField(blank=True)
+    text_osob2 = models.TextField(blank=True)
+    text_osob3 = models.TextField(blank=True)
 
--- infoversesite/infoversesite/settings.py
+++ infoversesite/infoversesite/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES = []
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
